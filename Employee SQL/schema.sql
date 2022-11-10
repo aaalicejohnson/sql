0e55d09c03010017796083e8ff4c727cd75d6e9c
Alice Johnson <aaalicejohnson@gmail.com>
2022-11-10 01:55:17 -0500

    schema edit

--- Employee SQL/schema.sql
+++ Employee SQL/schema.sql
@@ -1,4 +1,9 @@
-drop table if exists departments
+drop table if exists dept_emp;
+drop table if exists dept_manager;
+drop table if exists salaries; 
+drop table if exists employees;
+drop table if exists departments;
+drop table if exists titles;
 
 create table departments
 	(dept_no VARCHAR(4), 
